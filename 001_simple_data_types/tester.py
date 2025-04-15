209f1584d935f951b8bf2fb6ee17ad86161b15d7
Tõnis <tonislille@gmail.com>
2025-04-15 20:38:18 +0300

    strings, string methods

---
+++ 001_simple_data_types/tester.py
@@ -0,0 +1,58 @@
+"""
+This is a comment!
+"""
+
+# This is also a comment!
+# Ctrl + / (comment shortcut)
+
+# age = 35  # Integer (int)
+# height = 1.87 # Float (float)
+
+# print(height)
+# print(type(height))
+
+# print(9 % 2)  # 2 + 2 + 2 + 2 + 1
+# print(9 % 5)  # 5 + 4
+
+# print(9 / 2) # always returns float
+# print(9 // 2) # always returns int
+
+# empty_string = "" # empty string is still a string (str)
+
+# first_name = "Tonis" # String (str)
+# print(first_name)
+# print(type(first_name))
+# print("*" * 30)
+# print("Hello" + " " + "World" + "!")
+
+# x = 123.123 # Float
+# print(x) # Print float
+# print(int(x)) # Convert float to int
+# print(type(x)) # Type stays the same in the original "x"
+# print(int(float(x))) # x will be converted to a float and then into an int
+
+# is_popular = True # Boolean (bool)
+# is_not_popular = False # Boolean (bool)
+#
+# nothing = None # If you don't have a value
+#
+# x= 0
+#
+# print(bool(x))
+
+# # FALSE values
+# integer_val = 0 # 0 is always False
+# float_val = 0.00
+# string_val = "" # Empty string is always False
+# none_type = None
+
+""" TRIANGLE FORMULA"""
+#
+# side_a = float(input("Enter side A: "))
+# side_b = float(input("Enter side B: "))
+# side_c = float(input("Enter side C: "))
+#
+# semi_perimeter = (side_a + side_b + side_c) / 2
+# area = (semi_perimeter * (semi_perimeter - side_a) * (semi_perimeter - side_b) * (semi_perimeter - side_c)) ** 0.5
+#
+# print(area)
